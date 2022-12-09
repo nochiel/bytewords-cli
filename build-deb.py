6414be859e5cd8ae933939c14985efa9b6011eb3
Nick Ochiel <nochiel@users.noreply.github.com>
2022-12-10 02:49:02 +0300

    Fix build: argp is weird.

--- build-deb.py
+++ build-deb.py
@@ -12,6 +12,7 @@
 
 import os
 import subprocess
+import shutil
 
 env = dict(DEBEMAIL = '', DEBFULLNAME = '')
 
@@ -31,7 +32,8 @@ except subprocess.CalledProcessError as e:
 
 # Checkout the latest tag for this build.
 filename = f'{package_name}_{version}'
-subprocess.run(['git', 'worktree', 'add', '-f', f'../{package_name}-{version}', version])
+# subprocess.run(['git', 'worktree', 'add', '-f', f'../{package_name}-{version}', version])
+subprocess.run(['git', 'worktree', 'add', '-f', f'../{package_name}-{version}', 'build-deb'])
 os.chdir(f'../{package_name}-{version}')
 
 # Make a list of the files and directories we want. 
@@ -40,9 +42,51 @@ os.chdir(f'../{package_name}-{version}')
 # subprocess.run(['tar', 'cvzf', f'../{filename}.tar.gz', '.'])
 subprocess.run(['git', 'submodule', 'init'])
 subprocess.run(['git', 'submodule', 'update'])
+
+os.chdir('deps')
+
+os.chdir('argp-standalone')
+subprocess.run(['git', 'remote', 'add', 'nochiel', 'https://github.com/nochiel/argp-standalone.git'])
+subprocess.run(['git', 'fetch', 'nochiel', 'build-deb'])
+try:
+    subprocess.run(['git', 'checkout', 'nochiel/build-deb'])
+except Exception as e:
+    raise e
+
+os.chdir('..')
+
+os.chdir('bc-bytewords')
+subprocess.run(['git', 'remote', 'add', 'nochiel', 'https://github.com/nochiel/bc-bytewords.git'])
+subprocess.run(['git', 'fetch', 'nochiel', 'build-deb'])
+try:
+    subprocess.run(['git', 'checkout', 'nochiel/build-deb'])
+except Exception as e:
+        raise e
+
+os.chdir('..')
+# os.chdir('../bc-crypto-base')
+# subprocess.run(['git', 'remote', 'add', 'origin', 'https://github.com/nochiel/bc-crypto-base.git'])
+# subprocess.run(['git', 'checkout', 'build-deb'])
+
+# Create an archive to use for the package.
+os.chdir('..')
+package_directory = os.path.basename(os.getcwd())
+assert package_directory == f'{package_name}-{version}'
+
+# Remove files that shouldn't be in the package.
+try:
+    shutil.rmtree('.github')
+    shutil.rmtree('.vscode')
+except:
+    pass
+
+# os.chdir('..')
+# os.replace(package_directory, f'{package_directory}.old')
+# shutil.copytree(f'{package_directory}.old', package_directory)
+# os.chdir(package_directory)
+
 subprocess.run(['dh_make', '-y', '-s', '--createorig'],
                env = env)
 
 # TODO(nochiel) Install build dependencies and required dependencies.
-
 subprocess.run(['dpkg-buildpackage', '-us', '-uc'])
